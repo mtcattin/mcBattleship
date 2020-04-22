68ebfa3dec2e290f520fd9b1bbc3953eb1320b1b
mtcattin <cat16002@byui.edu>
2020-04-22 12:52:37 -0700

    Added initial JS commit with view

---
+++ mcBattleship.js
@@ -0,0 +1,26 @@
+var view = {
+    //This method & displays it in the message display area
+    displayMessage: function(msg) {
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+
+    displayHit: function(location) {
+        var cell = document.getElementById("location");
+        cell.setAttribute("class", "hit");
+    },
+
+    displayMiss: function(location) {
+        var cell = document.getElementById("location");
+        cell.setAttribute("class", "miss");
+    }    
+};
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+view.displayMessage("Testing Message Display");
