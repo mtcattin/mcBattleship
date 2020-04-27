cd1e32952f261bfd191e102cac5972cbc0e1f61d
mtcattin <cat16002@byui.edu>
2020-04-27 16:20:20 -0700

    Added Random Generator Logic to Model Object
    >generate new ship (verticle or horizontal)
    >generate ship on random location of board
    >collision check method

--- mcBattleship.js
+++ mcBattleship.js
@@ -63,6 +63,49 @@ var model = {
             }
         }
         return true;
+    },
+    generateShipLocations: function() {
+        var locations;
+        for(var i = 0; i < this.numShips; i++) {
+            do {
+                locations = this.generateShip();
+            } while(this.collision(locations));
+            this.ships[i].locations = locations;
+        }
+    },
+    generateShip: function() {
+        var direction = Math.floor(Math.random() * 2);
+        var row;
+        var col;
+        if(direction === 1) {
+            row = Math.floor(Math.random() * this.boardSize);
+            col = Math.floor(Math.random() * (this.boardSize - (this.shipLength + 1)));
+        } else {
+            row = Math.floor(Math.random() * (this.boardSize - (this.shipLength + 1)));
+            col = Math.floor(Math.random() * this.boardSize);
+                
+        }
+
+        var newShipLocations = [];
+        for(var i = 0; i < this.shipLength; i++) {
+            if(direction === 1) {
+                newShipLocations.push(row + "" + (col + i));
+            } else {
+                newShipLocations.push((row + i) + "" + col);
+            }
+        }
+        return newShipLocations;
+    },
+    collision: function(locations) {
+        for(var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            for(var j = 0; j < locations.length; j++) {
+                if(ship.locations.indexOf(locations[j]) >= 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 };
 /*
